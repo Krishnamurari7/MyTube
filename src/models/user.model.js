e13c6a01605b8e8b2fea6ba3e82856adfed62600
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-03-06 09:03:37 +0530

    create user model

---
+++ src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullname: {
+            type: String,
+            require: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, 
+            require: true
+        },
+        coverImage:{
+            type: String
+        },
+        watchHistory:[{
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }],
+        passwords: {
+            type: String,
+            require: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+export const user = mongoose.model("user", userSchema)

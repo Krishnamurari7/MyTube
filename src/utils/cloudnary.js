413978be38e6f9cc4c2cc507629c3cd99f80000c
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-03-08 14:32:32 +0530

    update cloudanary

--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -9,8 +9,27 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
-
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        //upload the file cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been upload successfully
+        console.log("file has been upload successfully",
+        response.url);
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath) //remove the localy save temporary file as the upload operation got failed
+        return null;
+    }
+}
 
 cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
   { public_id: "olympic_flag" }, 
   function(error, result) {console.log(result); });
+
+
+
+export {uploadOnCloudinary}

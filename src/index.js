643ba66e8601dc606dafc92ac036f571bd2666d5
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-03-11 12:04:05 +0530

    update and run

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 
 import dotenv from "dotenv"
 
-import mongoose, { Mongoose } from "mongoose";
+import { Mongoose } from "mongoose";
 import { DB_NAME } from "./constant.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'

2d4336d3b531a5b14f9e656e71f717f785c96d1e
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-03-13 20:55:19 +0530

    Update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,11 +8,11 @@ const asyncHandler = (requestHandler) => {
 export { asyncHandler }
 
 
+
+
 // const asyncHandler = () => {}
 // const asyncHandler = (func) => () => {}
 // const asyncHandler = (func) => async () => {}
-
-
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try{
 //         await fn(req, res, next)
